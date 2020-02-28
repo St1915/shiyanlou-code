0139670dc11ddf71eddc9c175c4abe8b5bcf210f
St1915 <61374306+St1915@users.noreply.github.com>
2020-02-28 18:25:38 +0800

    Add files via upload
    calculator2.0

--- calculator.py
+++ calculator.py
@@ -1,14 +1,13 @@
-income=int(input('请输入你的工资:'))
-salary=0
-shouldPay=0
-tax=0
+import sys
 
-def calculator(num):
-    shouldPay=num-5000
+
+def calculator(income):
+    insurance=0.08+0.02+0.005+0.06
+    shouldPay=income*(1-insurance)-5000
     if shouldPay <= 0:
         tax = 0
     elif 0<shouldPay<=3000:
-        tax = shoulPay * 0.03
+        tax = shouldPay * 0.03
     elif 0 < shouldPay <= 3000:
         tax = shouldPay * 0.03
     elif 3000 < shouldPay <= 12000:
@@ -23,7 +22,17 @@ def calculator(num):
         tax = shouldPay * 0.35 - 7160 
     else:
         tax = shouldPay * 0.45 -15160
-    salary=income - tax
+    salary=income*(1-insurance) - tax
     return'{:.2f}'.format(salary)
 
-print('你的税后收入是:{}'.format(calculator(income)))
+def main():
+    for item in sys.argv[1:]:
+        id,income =item.split(':')
+        try:
+            income=int(income)
+        except ValueError:
+            print('请在薪资的位置输入数字')
+            continue
+        print('{}:{}'.format(id,calculator(income)))
+if __name__ == '__main__':
+    main()
